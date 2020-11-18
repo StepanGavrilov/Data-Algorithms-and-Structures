7401c61d0ea7f0853d3ab1f10295fce51aa501c9
StepanGavrilov <gavrilovstepan78@gmail.com>
2020-11-18 23:41:36 +0300

    btree update

--- DataStructures3/b_tree.py
+++ DataStructures3/b_tree.py
@@ -55,24 +55,61 @@ class BinaryTree:
     def get_tree_height(self):
         pass
 
-    def remove(self):
-        pass
+    def remove_node(self, index: int):
+        # TODO search here !
+        previous_node = None
+        current_node = self.root
+        while current_node:
+            if index < current_node.index:
+                previous_node = current_node
+                current_node = current_node.left
+                if index == current_node.index:
+                    if current_node.left is not None and current_node.right is None:  # Если есть один потомок левый
+                        previous_node.left = current_node.left
+                        return
+                    if current_node.right is not None and current_node.left is None:  # Если есть один потомок правый
+                        previous_node.left = current_node.right
+                        return
+            if index > current_node.index:
+                previous_node = current_node
+                current_node = current_node.right
+                if index == current_node.index:
+                    if current_node.right is not None and current_node.left is None:  # Если есть один потомок правый
+                        previous_node.right = current_node.right
+                        return
+                    if current_node.left is not None and current_node.right is None:  # Если есть один потомок левый
+                        previous_node.right = current_node.left
+                        return
+                    return
 
-    def show_tree(self):
-        pass
+    def get_max(self) -> Node:
+        """Max index"""
+        current_node = self.root
+        while current_node.right is not None:
+            current_node = current_node.right
+        return current_node
+
+    def get_min(self) -> Node:
+        """Min index"""
+        current_node = self.root
+        while current_node.left is not None:
+            current_node = current_node.left
+        return current_node
+
+    def show_tree(self):                
+        print(self.root.index)
+        print(self.root.right.index)
+        print(self.root.right.right.index)
+        print(self.root.right.right.left.index)
 
 
 if __name__ == '__main__':
     bt = BinaryTree()
-    bt.add(25)
-    bt.add(20)
-    bt.add(17)
-    bt.add(1, 'yeah')
-    bt.add(15)
-    bt.add(35)
-    bt.add(34)
-    bt.add(45)
-    bt.add(44)
-    bt.add(60)
 
+    bt.add(25)
+    bt.add(30)
+    bt.add(55)
+    bt.add(45)
+
+    print('index', bt.get_min())
     bt.show_tree()
